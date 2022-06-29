3c57dee1a86291b746ee6535d8cdbe4b0da17e21
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-29 20:57:14 +0530

    create variables

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const listers = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+smallCups.forEach((cup, idx) => {
+  
+})
