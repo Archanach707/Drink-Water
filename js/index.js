2b8e943f37a306eac1dd3c64c6e577ee36d6ad8f
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-29 22:57:56 +0530

    make another function and also call

--- js/index.js
+++ js/index.js
@@ -4,5 +4,15 @@ const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
 smallCups.forEach((cup, idx) => {
-  
+  cup.addEventListener('click', () => highlightCups(idx))
 })
+
+function highlightCups(idx) {
+  if(smallCups[idx].classList.contains('full') && ! smallCups
+  [idx].nextElementSibling.classList.contains('full')) {
+    idx--
+  }
+
+  
+
+
