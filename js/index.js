3bb4a77aad164978ead4d7de9cdc5c32960952ad
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-30 10:02:52 +0530

    1. modification in html and css file, 2. drink water project complete

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const listers = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
+updateBigCup()
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx))
 })
@@ -13,6 +15,41 @@ function highlightCups(idx) {
     idx--
   }
 
+  smallCups.forEach((cup,idx2) => {
+    if(idx2 <= idx) {
+      cup.classList.add('full')
+    }else {
+      cup.classList.remove('full')
+    }
+
+  })
+
+  updateBigCup()
+}
   
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+  const totalCups = smallCups.length
+  
+  console.log(fullCups)
+  
+  if(fullCups === 0) {
+    percentage.style.visibility = 'hidden'
+    percentage.style.height = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${fullCups / totalCups *
+    330}px`
+    percentage.innerText = `${fullCups / totalCups * 100}%`
+  }
+
+  if(fullCups === totalCups) {
+    remained.style.visibility = 'hidden'
+    remained.style.height = 0
+  } else {
+    remained.style.visibility = 'visible'
+    listers.innerText = `${2 - (250 * fullCups / 1000)}`
+  }
+}
 
 
